e20dbe60bfc68764ed483302a099dfb84aebf6ae
Amir Shehata <shehataa@ornl.gov>
2026-03-11 12:50:51 -0400

    DEFw: TNQVM no longer needs check_active_tasks
    check_active_tasks() is defined in the util/qrc. No need to override it
    in TNQVM qpm.
    
    Signed-off-by: Amir Shehata <amir.shehata@ornl.gov>

--- python/services/svc_tnqvm_qpm/svc_qrc.py
+++ python/services/svc_tnqvm_qpm/svc_qrc.py
@@ -12,61 +12,6 @@ class QRC(UTIL_QRC):
 	def __init__(self, start=True):
 		super().__init__(start=start)
 
-	def check_active_tasks(self, wid):
-		complete = []
-		for task_info in self.worker_pool[wid]['active_tasks']:
-			try:
-				stdout, stderr, rc = task_info['launcher'].status(task_info['pid'])
-			except DEFwInProgress:
-				logging.debug(f"{task_info} still in progress")
-				continue
-			except Exception as e:
-				raise e
-			logging.debug(f"{task_info} completed")
-			complete.append(task_info)
-			circ = task_info['circ']
-			cid = circ.get_cid()
-			qasm_file = task_info['qasm_file']
-			if not rc:
-				logging.debug(f"Circuit {cid} successful")
-				try:
-					output_file = qasm_file+".result.r0"
-					with open(output_file, 'r') as f:
-						output = f.read()
-						output = yaml.safe_load(output)
-					os.remove(output_file)
-					circ.set_exec_done()
-				except Exception as e:
-					output = "{result: missing, exception: "+ f"{e}" + "}"
-					circ.set_fail()
-			else:
-				stdout = stdout.decode('utf-8')
-				stderr = stderr.decode('utf-8')
-				res = stdout + '\n' + stderr
-				output = "{result: "+ f"{res}" + "}"
-				circ.set_fail()
-
-			try:
-				os.remove(qasm_file)
-			except:
-				pass
-
-			r = {'cid': cid,
-				 'result': output,
-				 'rc': rc,
-				 'launch_time': circ.launch_time,
-				 'creation_time': circ.creation_time,
-				 'exec_time': circ.exec_time,
-				 'completion_time': circ.completion_time,
-				 'resources_consumed_time': circ.resources_consumed_time,
-				 'cq_enqueue_time': time.time(),
-				 'cq_dequeue_time': -1 }
-			with self.circuit_results_lock:
-				self.circuit_results.append(r)
-
-		for task_info in complete:
-			self.worker_pool[wid]['active_tasks'].remove(task_info)
-
 	def form_cmd(self, circ, qasm_file):
 		import shutil
 
